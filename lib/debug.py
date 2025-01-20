d5915b3ca5ee54d432bea885464ca7ba9af24e87
githinjisamson1 <githinjisamson148@gmail.com>
2025-01-20 06:33:07 +0300

    Add Freebie Model

--- lib/debug.py
+++ lib/debug.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 
 from models import Company, Dev
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///freebies.db')
-    import ipdb; ipdb.set_trace()
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///freebies.db")
+    import ipdb
+
+    ipdb.set_trace()
--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,21 +9,49 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+# Table Object
+company_devs = Table(
+    "company_devs",
+    Base.metadata,
+    Column("company_id", Integer, ForeignKey("companies.id"), primary_key=True),
+    Column("dev_id", Integer, ForeignKey("devs.id"), primary_key=True),
+    extend_existing=True,
+)
+
+
 class Company(Base):
-    __tablename__ = 'companies'
+    __tablename__ = "companies"
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship("Freebie", backref=backref("company"))
+    devs = relationship("Dev", secondary=company_devs, back_populates="companies")
+
     def __repr__(self):
-        return f'<Company {self.name}>'
+        return f"<Company {self.name}>"
+
 
 class Dev(Base):
-    __tablename__ = 'devs'
+    __tablename__ = "devs"
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
+
+    freebies = relationship("Freebie", backref=backref("dev"))
+    companies = relationship("Company", secondary=company_devs, back_populates="devs")
 
     def __repr__(self):
-        return f'<Dev {self.name}>'
+        return f"<Dev {self.name}>"
+
+
+class Freebie(Base):
+    __tablename__ = "freebies"
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    dev_id = Column(Integer(), ForeignKey("devs.id"))
+    company_id = Column(Integer(), ForeignKey("companies.id"))
